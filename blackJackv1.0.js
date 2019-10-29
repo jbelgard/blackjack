da22e2569988e31d44754f8f7347e304cd43993e
Jason Belgard <belgard.jason@gmail.com>
2019-10-28 23:51:39 -0500

    Further along

---
+++ blackJackv1.0.js
@@ -0,0 +1,69 @@
+"use strict";
+
+var numCardsPulled = 0;
+
+var player = {
+    cards: [],
+    score: 0,
+    money: 100
+};
+var dealer = {
+    cards: [],
+    score: 0
+};
+
+document.getElementById('player-money').innerHTML = 'Your money: $' + player.money;
+document.getElementById('hit-button').disabled = true;
+document.getElementById('stand-button').disabled = true;
+
+function getCardsValue(a) {
+    var cardArray = [],
+        sum = 0,
+        i = 0,
+        aceCount = 0;
+    cardArray = a;
+    for (i; i < cardArray.length; i += 1) {
+        if (cardArray[i].rank === ']' || cardArray[i].rank === 'Q' || cardArray[i].rank === 'K') {
+            sum += 10;
+        } else if (cardArray[i].rank === 'A') {
+            sum += 11;
+            aceCount += 1;
+        }else {
+            sum += cardArray[i].rank;
+        }
+    }
+    while (aceCount > 0 && sum > 21) {
+        sum -= 10;
+        aceCount -= 1;
+    }
+    return sum;
+}
+
+var deck = {
+    deckArray: [],
+    initialize: function () {
+        var suitArray, rankArray, s, r;
+        suitArray = ['clubs', 'diamonds', 'hearts', 'spades'];
+        rankArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+        for (s = 0; s < suitArray.length; s += 1) {
+            for (r = 0; r < rankArray.length; r += 1) {
+                this.deckArray[s * 13 + r] = {
+                    rank: rankArray[r],
+                    suit: suitArray[s]
+                };
+            }
+        }
+    },
+    shuffle: function () {
+        var temp, i, rnd;
+        for (i = 0; i < this.deckArray.length; i += 1) {
+            rnd = Math.floor(Math.random() * this.deckArray.length);
+            temp = this.deckArray[i];
+            this.deckArray[i] = this.deckArray[rnd];
+            this.deckArray[rnd = temp];
+        }
+    }
+};
+
+deck.initialize();
+deck.shuffle();
